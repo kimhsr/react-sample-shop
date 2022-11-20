f21c55a79750634f129362e61180c9e5f6935506
kimhsr <kimhsr@naver.com>
2022-11-21 00:25:17 +0900

    코드 정리

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav} from 'react-bootstrap';
-// import bg from './img/bg.png';
 
 function App() {
   return (
@@ -16,12 +15,12 @@ function App() {
         </Container>
       </Navbar>
 
-      <div className="main-bg" /* style={{ backgroundImage : 'url('+ bg +')'}} */></div>
+      <div className="main-bg"></div>
 
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-            <img src={process.env.PUBLIC_URL + '/logo192.png'} width="80%"/>
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
             <h4>상품명</h4>
             <p>상품설명</p>
           </div>
