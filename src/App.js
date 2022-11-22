051ed09f59f983fb1237f2a0d6dd52c688676cde
kimhsr <kimhsr@naver.com>
2022-11-22 15:32:43 +0900

    Routes, Route, Link 연습

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import { useState } from 'react';
 import data from './data.js';
+import { Routes, Route, Link } from 'react-router-dom'
 
 function App() {
 
@@ -10,29 +11,39 @@ function App() {
 
   return (
     <div className="App">
+
       <Navbar bg="dark" variant="dark" className="navbar">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Cart</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/detail">Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
-      <div className="main-bg"></div>
+      {/* <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link> */}
 
-      <div className="container">
-        <div className="row">
-          {
-            shoes.map(function(a, i){
-              return (
-                <Card shoes={shoes[i]} i={i}/>
-              )
-            })
-          }
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={
+          <div>
+            <div className="main-bg"></div>
+            <div className="container">
+              <div className="row">
+                {
+                  shoes.map(function (a, i) {
+                    return (
+                      <Card shoes={shoes[i]} i={i} />
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </div>
+        } />
+        <Route path="/detail" element={<div>상세페이지</div>} />
+      </Routes>
 
     </div>
   );
@@ -41,7 +52,7 @@ function App() {
 function Card(props) {
   return (
     <div className="col-md-4">
-      <img src={'https://codingapple1.github.io/shop/shoes'+ (props.i+1) +'.jpg'} width="80%" alt="shoes1" />
+      <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width="80%" alt={props.i} />
       <h4>{props.shoes.title}</h4>
       <p>{props.shoes.price}</p>
     </div>
