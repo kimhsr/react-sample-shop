9889f3c97611e1ccdc1bb3f3ff07180bdc0cdc3e
kimhsr <kimhsr@naver.com>
2022-11-23 16:17:58 +0900

    detail 페이지 이미지 수정

--- src/App.js
+++ src/App.js
@@ -19,8 +19,7 @@ function App() {
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
-            <Nav.Link onClick={() => { navigate('/detail') }}>Detail</Nav.Link>
-            <Nav.Link onClick={() => { navigate('/event') }}>Event</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/detail/0') }}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -42,7 +41,7 @@ function App() {
             </div>
           </div>
         } />
-        <Route path="/detail" element={<Detail />} />
+        <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
 
         <Route path="/about" element={<About />}>
           <Route path="member" element={<div>멤버</div>} />
