1f14805ae5c2f7d3ac8241457d7b630687413e4c
kimhsr <kimhsr@naver.com>
2022-11-24 16:03:42 +0900

    stlyed-components 연습

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,22 +1,40 @@
 import { useParams } from "react-router-dom";
+import styled from 'styled-components';
+
+let Box = styled.div`
+  padding : 20px;
+  background : grey;
+`;
+
+let YelloBtn = styled.button`
+  background : ${ props => props.bg };
+  color : ${ props => props.bg == 'blue' ? 'black' : 'white' };
+  padding : 10px;
+`;
+
+// let NewBtn = styled.button(YelloBtn)`
+
+// `
 
 function Detail(props) {
 
   let { id } = useParams();
-  let 찾은상품 = props.shoes.find(function(x){
-    return x.id == id;
-  })
 
   return (
     <div className="container">
+      <Box>
+        <YelloBtn bg="black">버튼</YelloBtn>
+        <YelloBtn bg="blue">버튼</YelloBtn>
+      </Box>
+
       <div className="row">
         <div className="col-md-6">
           <img src={'https://codingapple1.github.io/shop/shoes' + (parseInt(id) + 1) + '.jpg'} width="100%" />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{찾은상품.title}</h4>
-          <p>{찾은상품.content}</p>
-          <p>{찾은상품.price}원</p>
+          <h4 className="pt-5">{props.shoes[id].title}</h4>
+          <p>{props.shoes[id].content}</p>
+          <p>{props.shoes[id].price}원</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
