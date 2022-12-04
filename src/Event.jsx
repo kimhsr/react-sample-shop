62923fac11af070b75a48eee29023d0028074f16
kimhsr <kimhsr@naver.com>
2022-12-04 19:59:14 +0900

    성능개선 3 : useTransition, useDeferredValue

--- src/Event.jsx
+++ src/Event.jsx
@@ -1,12 +1,33 @@
+import React from "react";
+import { useState, useTransition, useDeferredValue } from "react";
 import { Outlet } from "react-router-dom";
 
+let a = new Array(10000).fill(0);
+
 function Event() {
+  let [name, setName] = useState("");
+  let [isPending, startTransition] = useTransition();
+  let state = useDeferredValue(name);
+
   return (
     <div>
       <h4>오늘의 이벤트</h4>
       <Outlet></Outlet>
+      <input
+        onChange={(e) => {
+          // 늦게 처리
+          startTransition(() => {
+            setName(e.target.value);
+          });
+        }}
+      ></input>
+      {isPending
+        ? "로딩중"
+        : a.map(() => {
+            return <div>{state}</div>;
+          })}
     </div>
-  )
+  );
 }
 
 export default Event;
