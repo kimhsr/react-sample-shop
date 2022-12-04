79a5e91369e82b32483fddf773b2491aa53438be
kimhsr <kimhsr@naver.com>
2022-12-04 18:03:19 +0900

    코드 정리

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,44 +1,18 @@
 // @ts-nocheck
-import React, { memo, useMemo, useState } from "react";
 import { Table } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { addCount } from "../store.js";
 import { increase } from "../store/userSlice";
 
-// 무거운 컴포넌트들에만 memo 사용 (대부분 붙일 일은 없음)
-// let Child = memo(function () {
-//   console.log("재렌더링");
-//   return <div>자식</div>;
-// });
-
-// function 함수(){
-//   return 반복문 여러번 돌린결과
-// }
-
 function Cart() {
-
-  // useMemo 컴포넌트 렌더링시 1회만 실행함
-  // let result = 함수();
-  // useMemo(()=>{
-  //   return 함수();
-  // }, [state]);
 
   let state = useSelector((state) => {
     return state;
   });
   let dispatch = useDispatch();
-  // let [count, setCount] = useState(0);
 
   return (
     <div>
-      {/* <Child count={count}></Child>
-      <button
-        onClick={() => {
-          setCount(count + 1);
-        }}
-      >
-        +
-      </button> */}
       <h5>
         {state.user.name} {state.user.age}의 장바구니
       </h5>
